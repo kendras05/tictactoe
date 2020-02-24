cdbee254425c20b7b7bf1c7c91791f55212a350f
kendras05 <kendrapsy@gmail.com>
2020-02-23 16:45:33 -0800

    tried to organize my code by organizing .js files in the components folder like we learned in class and putting css files in the assets folder however, i am realizing that i am working with js and not jsx files which means i am not running npx, i am doing npm so i cannot import files from different folders other than src without an error code. i am following along a tutorial while trying to incoporate organization learned from class

---
+++ src/Square.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './app.css'
+
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export default Square
