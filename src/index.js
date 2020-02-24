5b5560567d96c59d5c49f69a25c1001896798df6
kendras05 <kendrapsy@gmail.com>
2020-02-23 17:00:52 -0800

    finally imported all of the files correctly to make something appear for npm start

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import Game from './Game'
 import './app.css'
 
 
-function App() {
-  return (
-    <div className="App">
-			<Game />
-    </div>
-  );
-}
 
-export default App;
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
 
